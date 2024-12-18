430df7d49c927873b5bdea9dc788311c7c7e96df
monov999 <monov999@gmail.com>
2024-12-19 01:34:21 +0500

    Add files via upload

---
+++ РИС-24-3Б Монов Андрей  Лабораторная работа по рекурсиям/Рекурсия/Рекурсия/Рекурсия.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <clocale>
+#include <cmath>
+
+using namespace std;
+double S(double x, int n)
+{
+	if (n < 1)
+	{
+		return 0;
+	}
+	else
+	{
+		return (cos(2 * n * x) / (4 * pow(n, 2) - 1)) + S(x, n - 1);
+	}
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	double x;
+	int n;
+	cout << "Введите х и п: ";
+	cin >> x >> n;
+	cout << "S = " << S(x, n) << endl;
+	return 0;
+}
